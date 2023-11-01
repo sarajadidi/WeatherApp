d4d8a8c2789635785ef502c27c519f3f34bb3272
sarajadidi <sararivkajadidi@gmail.com>
2023-11-01 07:15:54 -0700

    made the output a little mroe creative

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -17,12 +17,33 @@ namespace WeatherApp
 
             var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIKey}&units=imperial";
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
+            //Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
+
+            double temperature = WeatherMap.GetTemp(apiCall);
+
+            Console.WriteLine();
+            Console.WriteLine($"It is currently {temperature} degrees F in your location.");
+
+            if (temperature < 32)
+            {
+                Console.WriteLine("It's very cold. Stay bundled up!");
+            }
+            else if (temperature < 50)
+            {
+                Console.WriteLine("It's a bit chilly. Consider wearing a jacket.");
+            }
+            else if (temperature < 70)
+            {
+                Console.WriteLine("The weather is mild. A light jacket might be enough.");
+            }
+            else
+            {
+                Console.WriteLine("It's warm. Enjoy the pleasant weather!");
+            }
         }
     }
-
 }
-
+   
 
